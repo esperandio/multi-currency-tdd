1c3670f4bb336eeba564fe2b97ed2194c443cfda
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-01-16 20:23:16 -0300

    test: remocao de efeitos colaterais de Dollar

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -2,8 +2,8 @@ import { Dollar } from './dollar'
 
 describe('Dollar', () => {
   it('should return 10 when five dollars area multiplied by two', () => {
-    const five = new Dollar(5)
-    five.times(2)
-    expect(five.amount).toEqual(10)
+    const five: Dollar = new Dollar(5)
+    const product: Dollar = five.times(2)
+    expect(product.amount).toEqual(10)
   })
 })
