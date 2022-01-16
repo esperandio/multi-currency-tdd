a915113e6e9efd71e016834696af93376e474093
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-01-16 20:17:08 -0300

    test: implementacao da rotina de multiplicacao

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,5 +1,9 @@
-describe('', () => {
-  test('', () => {
-    expect(1).toBe(1)
+import { Dollar } from './dollar'
+
+describe('Dollar', () => {
+  it('should return 10 when five dollars area multiplied by two', () => {
+    const five = new Dollar(5)
+    five.times(2)
+    expect(five.amount).toEqual(10)
   })
 })
