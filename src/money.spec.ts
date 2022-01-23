99b4c112258db0a076060ab09728053ab7fd1072
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-01-22 23:37:06 -0300

    test: tratamento para comparacao entre moedas diferentes

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -2,25 +2,29 @@ import { Dollar } from './dollar'
 import { Euro } from './euro'
 
 describe('Money', () => {
-  it('should return 10 when five dollars are multiplied by two and 15 when multiplied by three', () => {
+  it('should correctly handle dollar multiplication', () => {
     const five: Dollar = new Dollar(5)
     expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
     expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
   })
 
-  it('should return 10 when five euros are multiplied by two and 15 when multiplied by three', () => {
+  it('should correctly handle euro multiplication', () => {
     const five: Euro = new Euro(5)
     expect(five.times(2).equals(new Euro(10))).toBeTruthy()
     expect(five.times(3).equals(new Euro(15))).toBeTruthy()
   })
 
-  it('should return true when dollars of the same value are compared', () => {
+  it('should correctly handle dollar equality', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
   })
 
-  it('should return true when euros of the same value are compared', () => {
+  it('should correctly handle euro equality', () => {
     expect(new Euro(5).equals(new Euro(5))).toBeTruthy()
     expect(new Euro(5).equals(new Euro(6))).toBeFalsy()
+  })
+
+  it('should correctly handle different money equality', () => {
+    expect(new Euro(5).equals(new Dollar(5))).toBeFalsy()
   })
 })
