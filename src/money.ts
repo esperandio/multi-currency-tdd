abe67d96e76a52492718988fc92d8c746aa40826
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-01-22 23:36:56 -0300

    feat: tratamento para comparacao entre moedas diferentes

--- src/money.ts
+++ src/money.ts
@@ -2,6 +2,10 @@ export class Money {
   protected amount: number;
 
   equals (other: Money): boolean {
+    if (this.constructor !== other.constructor) {
+      return false
+    }
+
     return this.amount === other.amount
   }
 }
