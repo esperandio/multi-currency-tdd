82fec1fcee7a2b96184131e0e9cb8992cde84900
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-01-23 22:43:45 -0300

    test: remocao das classes Dollar e Euro

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -7,20 +7,9 @@ describe('Money', () => {
     expect(five.times(3).equals(Money.dollar(15))).toBeTruthy()
   })
 
-  it('should correctly handle euro multiplication', () => {
-    const five: Money = Money.euro(5)
-    expect(five.times(2).equals(Money.euro(10))).toBeTruthy()
-    expect(five.times(3).equals(Money.euro(15))).toBeTruthy()
-  })
-
   it('should correctly handle dollar equality', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
-  })
-
-  it('should correctly handle euro equality', () => {
-    expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy()
-    expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy()
   })
 
   it('should correctly handle different money equality', () => {
