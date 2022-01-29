c0fb23150a2ed18522ef197bf7e4c7f198f00e0d
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-01-29 19:42:13 -0300

    refactor: ajuste na descricao dos testes

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,22 +1,22 @@
 import { Money } from './money'
 
 describe('Money', () => {
-  it('should correctly handle dollar multiplication', () => {
+  it('should handle dollar multiplication', () => {
     const five: Money = Money.dollar(5)
     expect(five.times(2).equals(Money.dollar(10))).toBeTruthy()
     expect(five.times(3).equals(Money.dollar(15))).toBeTruthy()
   })
 
-  it('should correctly handle dollar equality', () => {
+  it('should handle dollar equality', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
   })
 
-  it('should correctly handle different money equality', () => {
+  it('should handle different money equality', () => {
     expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy()
   })
 
-  it('should correctly handle currencies', () => {
+  it('should handle currencies', () => {
     expect(Money.dollar(1).currency()).toEqual('USD')
     expect(Money.euro(1).currency()).toEqual('EUR')
   })
