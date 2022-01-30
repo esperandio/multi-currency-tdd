c0577a5e8093728d6558d0c69567413362d808cf
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-01-30 00:19:32 -0300

    test: implementacao de logica para reduzir uma expressao matematica para um valor de Money

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,4 +1,7 @@
+import { Bank } from './bank'
+import { Expression } from './expression'
 import { Money } from './money'
+import { Sum } from './sum'
 
 describe('Money', () => {
   it('should handle dollar multiplication', () => {
@@ -19,5 +22,34 @@ describe('Money', () => {
   it('should handle currencies', () => {
     expect(Money.dollar(1).currency()).toEqual('USD')
     expect(Money.euro(1).currency()).toEqual('EUR')
+  })
+
+  it('should handle simple addition', () => {
+    const five = Money.dollar(5)
+    const sum: Expression = five.plus(five)
+    const bank = new Bank()
+    const reduced: Money = bank.reduce(sum, 'USD')
+    expect(reduced.equals(Money.dollar(10))).toBeTruthy()
+  })
+
+  it('should return a sum when plus is used', () => {
+    const five = Money.dollar(5)
+    const result: Expression = five.plus(five)
+    const sum = result as Sum
+    expect(sum.augend()).toEqual(five)
+    expect(sum.addend()).toEqual(five)
+  })
+
+  it('should reduce a sum', () => {
+    const sum: Expression = new Sum(Money.dollar(3), Money.dollar(4))
+    const bank = new Bank()
+    const result = bank.reduce(sum, 'USD')
+    expect(result).toEqual(Money.dollar(7))
+  })
+
+  it('should reduce from a money', () => {
+    const bank = new Bank()
+    const result: Money = bank.reduce(Money.dollar(1), 'USD')
+    expect(result).toEqual(Money.dollar(1))
   })
 })
